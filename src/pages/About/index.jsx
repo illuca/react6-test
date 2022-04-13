b28ac99d6f9a1ad0f4dee2b4c43e1ec70a38ab89
Yuan Long <longyuan@magiccreative.tech>
2022-04-13 23:20:46 +0800

    Developer interface: update resolution, get solution list, get resolution detail, get all labels

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About(props) {
+    return (
+        <>
+            <h2>我是About组件</h2>
+        </>
+    );
+}
+
+export default About;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+function Home(props) {
+    return (
+        <>
+            <h2>我是Home组件</h2>
+        </>
+    );
+}
+
+export default Home;
