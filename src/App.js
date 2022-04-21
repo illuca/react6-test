6ec2749d51bc42b52bf97ef5cd568b6fcc90a2ef
Yuan Long <longyuan@magiccreative.tech>
2022-04-21 23:11:22 +0800

    安装react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>React Demo</h1>
+            <NavLink to={"/home"}>Home</NavLink>
+            <NavLink to={"/about"}>About</NavLink>
+            <div>
+                <Routes>
+                    <Route path={"/home"} element={<Home/>}></Route>
+                    <Route path={"/about"} element={<About/>}></Route>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
